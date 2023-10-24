da8d7aab22a37f9ff93c0c8f743a8e22758dbcdf
Bogdan Tishenko <95874565+Tezlaa@users.noreply.github.com>
2023-10-24 16:19:07 +0300

    Update settings.py fix bug with bool variable

--- src/config/settings.py
+++ src/config/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv('DEBUG'))
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(', ')
 
@@ -85,7 +85,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-DOCKER_RUN = bool(os.getenv('DOCKER_RUN'))
+DOCKER_RUN = os.getenv('DOCKER_RUN') == 'True'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
